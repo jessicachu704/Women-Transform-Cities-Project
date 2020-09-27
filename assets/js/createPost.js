6308710dcbafeb137e6f5737bfb9e0d726339cf0
pkaur740 <panesarpardeep0@gmail.com>
2020-09-27 09:42:59 -0700

    createPost finally writing data to the database

--- assets/js/createPost.js
+++ assets/js/createPost.js
@@ -1,20 +1,24 @@
-  
-    function createPost() {
+  function createPost() {
     console.log("hi");
     var subject1 = document.getElementById("subject").value;
     var msg1 = document.getElementById("message").value;
 
-        alert(subject1);
-        alert(msg1);
-       // if the current user logged in user
-      // is authenticated, then grab "uid" "displayName" and "email"
-      // use "set()" with merge (if document did not exist it will be created)
 
-     
-          db.collection("postings").doc(subject1).set({
-              description: subject1,
-              title: msg1,
-          }, {
-              merge: true
-          });
+    alert(subject1);
+    alert(msg1);
+    // if the current user logged in user
+    // is authenticated, then grab "uid" "displayName" and "email"
+    // use "set()" with merge (if document did not exist it will be created)
+
+    db.collection("postings").doc(subject1).set({
+        description: subject1,
+        title: msg1,
+        likes: 0
+    })
+    .then(function(){
+    console.log("doc successfully written");
+})
+.catch(function(error){
+    console.error("error: ", error);
+});
     }
