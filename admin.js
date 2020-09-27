3c1143a503acb3212b7827a9d42f5a486e1d9b34
Iris Law <irislawly@gmail.com>
2020-09-26 17:23:25 -0700

    added admin retrieve from data base postings

--- admin.js
+++ admin.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     updateUsername();
-
+    getPosts();
   });
   
   function updateUsername() {
@@ -18,4 +18,64 @@ $(document).ready(function () {
     });
   };
   
- 
+  function getPosts() {
+    firebase.auth().onAuthStateChanged(function (user) {
+      db.collection("postings")
+      .get()
+      .then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+              // doc.data() is never undefined for query doc snapshots
+              console.log(doc.id, " => ", doc.data());
+              
+              let title= doc.data().title;
+              let description = doc.data().description;
+              let category = doc.data().category;
+              let dateposted= doc.data().dateposted;
+              let likes= doc.data().likes;
+              let userid= doc.data().userid;
+              console.log(category , title, description);
+              createPost(title, description, category, dateposted, likes);
+
+              
+
+          });
+      })
+      .catch(function(error) {
+          console.log("Error getting documents: ", error);
+      });
+  
+  })
+  
+}
+  function createPost(title, description, category, dateposted, likes) {
+    let box = document.getElementById("thoughts");
+    let col = document.createElement("div");
+    let colBox = document.createElement("div");
+    let heading1 =  document.createElement("h4");
+    let heading2 = document.createElement("h4");
+    let paragraph = document.createElement("p");
+    let like = document.createElement("p");
+    let date = document.createElement("p");
+    let button = document.createElement("button");
+
+    col.setAttribute("class","col-lg-4 col-md-6 d-flex align-items-stretch");
+    col.setAttribute("data-aos","zoom-in");
+    col.setAttribute("data-aos-delay","100");
+    button.setAttribute("type", "button");
+    colBox.setAttribute("class", "icon-box");
+    heading1.innerHTML = title;
+    paragraph.innerHTML = description;
+    heading2.innerHTML = category;
+    like.innerHTML = likes + " likes";
+    button.innerHTML = "Delete"
+    date.innerHTML = dateposted.toDate();
+    colBox.appendChild(heading1);
+    colBox.appendChild(heading2);
+    colBox.appendChild(paragraph);
+    colBox.appendChild(like);
+    colBox.appendChild(date);
+    colBox.appendChild(button);
+    col.appendChild(colBox);
+    box.appendChild(col);
+
+  }
